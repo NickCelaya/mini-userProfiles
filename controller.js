e6f50a9a3ce16769283b77069ee800ad24019506
Nick Celaya <NickCelaya07@gmail.com>
2017-03-28 14:20:18 -0600

    Project complete

--- controller.js
+++ controller.js
@@ -1,5 +1,29 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
-    $scope.thisAppIsBroken = "This angular app is working";
-   
-})
+angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+
+    $scope.thisAppIsBroken = "This angular app is working Nick Celaya";
+
+    console.log($scope.thisAppIsBroken);
+
+
+
+
+    $scope.getUsers = function() {
+        $scope.users = mainService.getUsers();
+    }
+    $scope.getUsers();
+
+    // The above function is a way to get data to the view, only when invoked
+
+    $scope.users = mainService.getUsers();
+
+    // $scope.users will automatically put the data on view
+
+    $scope.toggleFavorite = mainService.toggleFavorite;
+
+
+
+
+
+
+    // end of controller
+});
